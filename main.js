50366902504de35fc6c110d8d5299bc65799ae1c
manueelaa123456 <manuela.fadrique@escola.pr.gov.br>
2026-05-07 15:03:58 +0000

    aula 4

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 <script src="main.js"></script>
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
